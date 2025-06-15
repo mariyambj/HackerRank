ff35143d7bff9754e9fabda1e0bcbfd990ad08fe
mariyam <mariyam.biju10@gmail.com>
2025-06-15 10:13:27 +0530

    nextone

---
+++ GameOfthrones.py
@@ -0,0 +1,20 @@
+
+
+
+def gameOfThrones(s):
+    char = []
+    for i in s:
+        if i not in char:
+            char.append(i)
+        else:
+            char.remove(i)
+    
+    if len(char) <= 1:
+        return "YES"
+    else:
+        return "NO"
+
+s = input("Enter the string: ")
+print(gameOfThrones(s))
+
+
