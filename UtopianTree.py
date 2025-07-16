2388380094ae6e0d05db8a44b4a935783dff7374
mariyam <mariyam.biju10@gmail.com>
2025-07-16 14:42:57 +0530

    Progressing

---
+++ UtopianTree.py
@@ -0,0 +1,83 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'utopianTree' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def utopianTree(n):
+    k=0
+    for i in range(0,n+1):
+        if i%2==0:
+            k+=1
+        if i%2!=0:
+            k*=2
+    return k
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = utopianTree(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
+
+
+'''The Utopian Tree goes through 2 cycles of growth every year. Each spring, it doubles in height. Each summer, its height increases by 1 meter.
+
+A Utopian Tree sapling with a height of 1 meter is planted at the onset of spring. How tall will the tree be after  growth cycles?
+
+For example, if the number of growth cycles is , the calculations are as follows:
+
+Period  Height
+0          1
+1          2
+2          3
+3          6
+4          7
+5          14
+Function Description
+
+Complete the utopianTree function in the editor below.
+
+utopianTree has the following parameter(s):
+
+int n: the number of growth cycles to simulate
+Returns
+
+int: the height of the tree after the given number of cycles
+Input Format
+
+The first line contains an integer, , the number of test cases.
+ subsequent lines each contain an integer, , the number of cycles for that test case.
+
+Constraints
+
+
+
+Sample Input
+
+3
+0
+1
+4
+Sample Output
+
+1
+2
+7'''
