50e22a83eccc896621d3a66b64215a550893f051
mariyam <mariyam.biju10@gmail.com>
2025-07-21 08:25:19 +0530

    Progressing

---
+++ EqualizetheArray.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'equalizeArray' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def equalizeArray(arr):
+    # Write your code here
+    count_arr={}
+    n=len(arr)
+    for i in arr:
+        count_arr[i]=arr.count(i)
+    return (n-max(count_arr.values()))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = equalizeArray(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
