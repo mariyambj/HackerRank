27d2e9b551c0feb9a971b46177fcbfe04f54e9ca
mariyam <mariyam.biju10@gmail.com>
2025-08-12 08:08:44 +0530

    Progressing

---
+++ FairRation.py
@@ -0,0 +1,53 @@
+b=[2,3,4,5,6]
+count=0
+if sum(b)%2!=0:
+    print("No")
+else:
+    for i in range(len(b)-1):
+        if b[i]%2!=0:
+            b[i]+=1
+            b[i+1]+=1
+            count+=2
+    print(str(count))
+
+
+
+    '''You are the benevolent ruler of Rankhacker Castle, and today you're distributing bread. Your subjects are in a line, and some of them already have some loaves. Times are hard and your castle's food stocks are dwindling, so you must distribute as few loaves as possible according to the following rules:
+
+Every time you give a loaf of bread to some person , you must also give a loaf of bread to the person immediately in front of or behind them in the line (i.e., persons  or ).
+After all the bread is distributed, each person must have an even number of loaves.
+Given the number of loaves already held by each citizen, find and print the minimum number of loaves you must distribute to satisfy the two rules above. If this is not possible, print NO.
+
+Example
+
+We can first give a loaf to  and  so .
+Next we give a loaf to  and  and have  which satisfies our conditions.
+All of the counts are now even numbers. We had to distribute  loaves.
+
+Function Description
+
+Complete the fairRations function in the editor below.
+
+fairRations has the following parameter(s):
+
+int B[N]: the numbers of loaves each persons starts with
+Returns
+
+string: the minimum number of loaves required, cast as a string, or 'NO'
+Input Format
+
+The first line contains an integer , the number of subjects in the bread line.
+
+The second line contains  space-separated integers .
+
+Constraints
+
+, where 
+Output Format
+
+Sample Input 0
+
+STDIN       Function
+-----       --------
+5           B[] size N = 5
+2 3 4 5 6   B = [2, 3, 4, 5, 6]   '''
