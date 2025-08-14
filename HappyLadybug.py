185462c1c47626464407c557093bb0e555947211
mariyam <mariyam.biju10@gmail.com>
2025-08-14 20:40:45 +0530

    Progressing

--- HappyLadybug.py
+++ HappyLadybug.py
@@ -2,27 +2,18 @@ from collections import Counter
 b='RBY_YBR'
 b=list(b)
 freq=Counter(b)
-bug=False
-for item,count in freq.items():
-    if item !='_' and count==1:
-        bug=True
-        break
-if '_' in b:
-    if bug:
+if '_' not in b:
+    for i in range(len(b)):
+        left=(i>0 and b[i]==b[i-1])
+        right=(i<len(b)-1 and b[i]==b[i+1])
+        if not (left or right):
+            print("NO")
+    print("YES")
+for i in freq:
+    if i!='_' and freq[i]==1:
         print("No")
-    else:
-        print("Yes")
-else:
-    i=0
-    while i <len(b):
-        count=1
-        while i+1<len(b) and b[i]==b[i+1]:
-            i+=1
-            count+=1
-        if count==1:
-            print("No")
-        i+=1
-    print("yes")
+print("YES")
+
 
 
 
