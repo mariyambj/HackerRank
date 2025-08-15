7ecc8db917f0faa726ac1a6c9b46d9ddd7980876
mariyam <mariyam.biju10@gmail.com>
2025-08-15 18:44:32 +0530

    Progressing

---
+++ StrangeCounter.py
@@ -0,0 +1,27 @@
+import sys
+
+#
+# Complete the 'strangeCounter' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER t as parameter.
+#
+
+def strangeCounter(t):
+    time = 1
+    value = 3
+    while time + value <= t:
+        time += value
+        value = 2 * value
+    return value - (t - time)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    result = strangeCounter(t)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
