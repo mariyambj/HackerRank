2aa117656c5e0f5c1bf3c3d31efadf37b1582b95
mariyam <mariyam.biju10@gmail.com>
2025-08-18 20:30:38 +0530

    Progressing

--- Superreducedstring.py
+++ Superreducedstring.py
@@ -1,11 +1,37 @@
-def super_reduced_string(s):
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'superReducedString' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def superReducedString(s):
     stack=[]
     for char in s:
         if stack and stack[-1]==char:
             stack.pop()
         else:
             stack.append(char)
-    return ''.join(stack)
+    if stack:
+        return ''.join(stack)
+    else:
+        return "Empty String"
 
-string="aaabccddd"
-print(super_reduced_string(string))
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = superReducedString(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
