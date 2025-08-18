55302185116d3702b27db1380207e95b2bdbe2cf
mariyam <mariyam.biju10@gmail.com>
2025-08-18 21:15:25 +0530

    Progressing

---
+++ Twocharacters.py
@@ -0,0 +1,15 @@
+"""def alternate(s):
+    dict={}
+    for char in s:
+        if char not in dict:
+            dict[char]=1
+        else:
+            dict[char]+=1
+        #values=list(dict.values())
+        #return values[-1]+values[-2]
+    return dict
+
+s = 'abaacdabd'
+print(alternate(s))"""
+
+
